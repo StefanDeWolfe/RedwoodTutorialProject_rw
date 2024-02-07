cb7359aef837c878d493335fe325f6ec90502ab2
Stefan DeWolfe <stefandewolfe@gmail.com>
2024-02-07 14:42:01 -0800

    Used the rw generators to generate layouts, components, and pages.

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,17 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/edit-profile" page={EditProfilePage} name="editProfile" />
+      <Route path="/profile" page={ProfilePage} name="profile" />
+      <Route path="/comments" page={CommentsPage} name="comments" />
+      <Route path="/favorites" page={FavoritesPage} name="favorites" />
+      <Route path="/privacy" page={PrivacyPage} name="privacy" />
+      <Route path="/disclaimers" page={DisclaimersPage} name="disclaimers" />
+      <Route path="/terms" page={TermsPage} name="terms" />
+      <Route path="/link" page={LinkPage} name="link" />
+      <Route path="/submit-link" page={SubmitLinkPage} name="submitLink" />
+      <Route path="/latest" page={LatestPage} name="latest" />
+      <Route path="/feed" page={FeedPage} name="feed" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
